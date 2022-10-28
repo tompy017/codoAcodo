f394b97554b5a2642f7fa543a062ed90849d2a20
Tomas Borelli <tomasborelli@protonmail.com>
2022-10-28 17:04:35 -0300

    .gitignore; js vars

---
+++ js/functionTickets.js
@@ -0,0 +1,18 @@
+var fullTicket = 200
+var descuentoEstudiantes = 0.8      // 80% descuento
+var descuentoTrainee = 0.5          // 50% descuento
+var descuentoJunior = 0.15          // 15% descuento
+
+var ticket_price = document.getElementById("valor-de-ticket");
+ticket_price.innerHTML += fullTicket;        // insert price on page
+
+
+var categorias = document.getElementById("categorias");
+
+
+
+function calc_price(){
+
+
+
+}
