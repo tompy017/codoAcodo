c34d53308bae74589f52c29e51d6cb3fce548558
tomas <tomasborelli@protonmail.com>
2022-11-05 21:30:19 -0300

    cambio de nombres, reparo falta de valor en tickets

--- js/functionTickets.js
+++ js/functionTickets.js
@@ -1,54 +1,48 @@
-var fullTicket = 200
-var descuentoEstudiantes = 0.8      // 80% descuento
-var descuentoTrainee = 0.5          // 50% descuento
-var descuentoJunior = 0.15          // 15% descuento
+var fullTicket = 200;
+var descuentoEstudiantes = 0.8;      // 80% descuento
+var descuentoTrainee = 0.5;          // 50% descuento
+var descuentoJunior = 0.15;          // 15% descuento
 
-var ticket_price = document.getElementById("valor-de-ticket");
-ticket_price.innerHTML += fullTicket;        // insert price on page
+var ticket_price = document.getElementById("ticket_price");
+ticket_price.innerHTML += fullTicket.toString();        // insert price on page
 
-var total
 
-var informarTotal = document.getElementById("total-a-pagar")
-var totalFinal = document.getElementById("informar-total")
-var cantidadTickets = parseInt(document.getElementById("cantidad").value);
-var userOption = parseInt(document.getElementById("categorias").value);     // Categoria seleccionada
+var total = 0;
+
+var informarTotal = document.getElementById("informarTotal")
+var totalFinal = document.getElementById("totalFinal")
+var cantidadTickets = parseInt(document.getElementById("cantidadTickets").value);
+var categorias = parseInt(document.getElementById("categorias").value);     // Categoria seleccionada
 var resumeBtn = document.getElementById("boton-resumen")
 var borrarBtn = document.getElementById("boton-borrar")
 
+
+
 function calc_price(){
 
-/*     var informarTotal = document.getElementById("total-a-pagar")
-    var totalFinal = document.getElementById("informar-total")
-    var cantidadTickets = parseInt(document.getElementById("cantidad").value);
-    var userOption = parseInt(document.getElementById("categorias").value);     // Categoria seleccionada
-    var resumeBtn = document.getElementById("boton-resumen")
-    var borrarBtn = document.getElementById("boton-borrar") */
-
-
     // Sin descuento
-    if (userOption === 1){
+    if (categorias === 1){
         total = fullTicket * cantidadTickets;
     }
     // Estudiante       
-    if (userOption === 2){                                                        
+    if (categorias === 2){                                                        
         total = (fullTicket * (1 - descuentoEstudiantes)) * cantidadTickets;
     }
     // Trainee
-    if (userOption === 3){
+    if (categorias === 3){
         total = (fullTicket * (1 - descuentoTrainee)) * cantidadTickets;
     }
     // Junior
-    if (userOption === 4){
+    if (categorias === 4){
         total = (fullTicket * (1 - descuentoJunior)) * cantidadTickets;
     }
 
     totalFinal.innerHTML += total;
     informarTotal.style.visibility="visible";
 
+
 }
 
 resumeBtn.addEventListener("click",calc_price)
-// borrarBtn.addEventListener("click", () => {
-//     informarTotal.style="hidden";
-//     tinformarTotal.innerHTML = "Total a pagar: $";
-// })
+
+console.log()
